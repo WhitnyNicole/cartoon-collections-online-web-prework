d408601159eadde88b82e4ca40b14cb4748a13c2
Whitny Edwards <wne4e2@gmail.com>
2018-11-20 00:37:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,6 @@ def summon_captain_planet# code an argument here
     veggies.collect do |call| 
     call.capitalize + "!"
   end
-end
 
 def long_planeteer_calls# code an argument here
   # Your code here
