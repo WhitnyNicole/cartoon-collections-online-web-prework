f5741514d7d0e266667ce9e975061e4094e341fc
Whitny Edwards <wne4e2@gmail.com>
2018-11-20 00:37:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,19 +3,20 @@ def roll_call_dwarves# code an argument here
     dwarfs.each_with_index do | name , index |
     puts "#{index + 1} #{name}"
   end
-
+end
 
 def summon_captain_planet# code an argument here
   # Your code here
     veggies.collect do |call| 
     call.capitalize + "!"
   end
+end
 
 def long_planeteer_calls# code an argument here
   # Your code here
    answer = false
-    long_planteer_calls.each do |call|
-  if call.length > 4
+  long_planteer_calls.each do |call|
+    if call.length > 4
       answer = true
     end
 end
@@ -25,4 +26,5 @@ def find_the_cheese# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese.find do |cheese|
     cheese_types.include?(cheese)
+  end
 end
