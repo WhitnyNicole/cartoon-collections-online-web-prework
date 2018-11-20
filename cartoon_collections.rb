43b71bf3c818d68a836cad18cc2d55918cce7e60
Whitny Edwards <wne4e2@gmail.com>
2018-11-20 00:41:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,8 @@ def long_planeteer_calls(long_planeteer_calls) # code an argument here
     if call.length > 4
       answer = true
     end
+  end
+    answer
 end
 
 def find_the_cheese(cheese)# code an argument here
